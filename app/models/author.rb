aaeec946e3abf1eac91591917803ee44ce686725
Martin Fenner <fenner.martin@mh-hannover.de>
2011-09-05 16:25:10 +0000

    Please enter the commit message for your changes. Lines starting

--- app/models/author.rb
+++ app/models/author.rb
@@ -55,7 +55,15 @@ class Author < ActiveRecord::Base
   end
   
   def articles_count
-    self.articles.count.to_s
+    self.articles.count
+  end
+  
+  def self.fetch_properties(author, options={})
+    
+    url = "http://academic.research.microsoft.com/json.svc/search?AppId=#{APP_CONFIG['mas_app_id']}&ResultObjects=Author&AuthorID=#{author.mas_id}&StartIdx=1&EndIdx=1"
+    Rails.logger.info "Microsoft Academic Search query: #{url}"
+    
+    properties = get_json(url, options)["d"]["Author"]["Result"][0]
   end
   
   def self.fetch_articles(author, options={})
