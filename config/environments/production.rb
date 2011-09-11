f61de1c223f8f4dd493b303670489def8e51a0db
Martin Fenner <fenner.martin@mh-hannover.de>
2011-09-11 10:17:15 +0000

    Temporarily changed production settings for troubleshooting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-config.cache_classes = true
+config.cache_classes = false
 
 # Enable threaded mode
 # config.threadsafe!
@@ -11,8 +11,8 @@ config.cache_classes = true
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
-config.action_controller.perform_caching             = true
+config.action_controller.consider_all_requests_local = true
+config.action_controller.perform_caching             = false
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
