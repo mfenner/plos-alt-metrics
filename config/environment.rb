d2c71f086fd276a02e101bc395eb44f01bc9af5c
Martin Fenner <fenner.martin@mh-hannover.de>
2011-09-11 00:29:52 +0000

    Moved will_paginate to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -59,6 +59,8 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+  
+  config.gem 'will_paginate', :version => '~> 2.3.16'
 end
 
 require Rails.root + 'lib/log4j_style_logger'
