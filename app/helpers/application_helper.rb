038775a08df954a9a3a4ae221784d644c93ff4b6
Martin Fenner <fenner.martin@mh-hannover.de>
2011-09-11 14:26:01 +0000

    Change sort of source statistics in author/source view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,7 +28,7 @@ module ApplicationHelper
   end
   
   def active_groups
-    @groups = Group.find :all, :conditions => ["sources.active=1"], :include => :sources
+    @groups = Group.find :all, :conditions => ["sources.active=1"], :include => :sources, :order => :group_id
   end
 
 end
