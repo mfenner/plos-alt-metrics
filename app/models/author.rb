44d33b5cfc4549482bdbc33adbc3dbfecc3ceb9f
Martin Fenner <fenner.martin@mh-hannover.de>
2011-10-01 08:39:55 +0000

    Update author.articles when mas id is changed

--- app/models/author.rb
+++ app/models/author.rb
@@ -21,7 +21,7 @@ class Author < ActiveRecord::Base
   
   has_many :positions
   has_many :authentications
-  has_many :contributions
+  has_many :contributions, :dependent => :destroy
   has_many :articles, :through => :contributions
   has_many :members
   has_many :groups, :through => :members
@@ -37,8 +37,8 @@ class Author < ActiveRecord::Base
   
   attr_accessible :username, :name, :mas, :mendeley, :remember_me
   
-  validates_numericality_of :mas, :allow_nil => true
-  validates_uniqueness_of :mas, :allow_nil => true
+  validates_numericality_of :mas, :allow_nil => true, :allow_blank => true
+  validates_uniqueness_of :mas, :allow_nil => true, :allow_blank => true
   validates_presence_of :username
   validates_uniqueness_of :username
   
@@ -189,14 +189,14 @@ class Author < ActiveRecord::Base
   end
   
   def self.fetch_articles(author, options={})
-    # Fetch articles, return nil if no response 
-    return nil if author.mas.blank?
+    # Fetch articles, return empty array if no mas identifier, no response, or no articles found
+    return [] if author.mas.blank?
     
     url = "http://academic.research.microsoft.com/json.svc/search?AppId=#{APP_CONFIG['mas_app_id']}&ResultObjects=Publication&PublicationContent=AllInfo&AuthorID=#{author.mas}&StartIdx=1&EndIdx=50"
     Rails.logger.info "Microsoft Academic Search query: #{url}"
     
     result = SourceHelper.get_json(url, options)["d"]["Publication"]
-    return nil if result.nil?
+    return [] if result.nil?
     
     articles = result["Result"]
   end
