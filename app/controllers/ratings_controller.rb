979dc9f2eb1c978a365a3092dc3db469029ac24d
Martin Fenner <fenner.martin@mh-hannover.de>
2011-12-27 01:27:27 +0000

    Switched from Prototype to jquery, switched to Highcharts for visualization

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -44,17 +44,17 @@ class RatingsController < ApplicationController
       f.options[:xAxis] = { :categories => ['Methods', 'Data', 'Conclusions'], :tickLength => 0 }
       f.options[:yAxis] = { :max => @posts_with_ratings.count, :lineWidth => 1, :gridLineWidth => 0, :title => nil, :labels  => { :enabled => true }, :tickInterval => @posts_with_ratings.count, :showFirstLabel => false }
       f.options[:title][:text] = nil
-      f.options[:plotOptions] = { :column => { :pointWidth => 70, :borderWidth => 0, :pointPadding => 0.25 } }
+      f.options[:plotOptions] = { :column => { :pointWidth => 60, :borderWidth => 0 } }
       f.options[:legend] = false
     end 
     
     # Calculate rating activity by day
     days = Rating.order('created_at').group('DATE(created_at)').count
     @activitychart = LazyHighCharts::HighChart.new('chart') do |f|
-      f.chart({:defaultSeriesType=>"spline", :width => 750, :height => 150 } )
+      f.chart({:defaultSeriesType=>"spline", :width => 770, :height => 150, :marginRight => 20 } )
       f.series(:name => "Ratings", :color => "#95ab63", :data => days.map {|a| [Date.strptime(a[0], '%Y-%m-%d').to_time.to_i * 1000, a[1]] })
-      f.options[:xAxis] = { :type => 'datetime', :tickLength => 0, :labels  => { :enabled => false } }
-      f.options[:yAxis] = { :max => @posts_with_ratings.count, :lineWidth => 1, :gridLineWidth => 0, :title => nil, :labels  => { :enabled => false }}
+      f.options[:xAxis] = { :type => 'datetime', :tickLength => 0, :labels  => { :enabled => true } }
+      f.options[:yAxis] = { :max => @posts_with_ratings.count, :lineWidth => 1, :gridLineWidth => 0, :title => nil, :labels  => { :enabled => true }, :tickInterval => @posts_with_ratings.count, :showFirstLabel => false }
       f.options[:title][:text] = nil
       f.options[:legend] = false
       f.options[:plotOptions] = { :column => { :connectNulls => true } }
