59802c981a7a308ce25c4ecb1eb5a9da304870cc
Martin Fenner <fenner.martin@mh-hannover.de>
2012-01-20 15:40:09 +0000

    Moved mobile stylesheet to sass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ PlosAltMetrics::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
