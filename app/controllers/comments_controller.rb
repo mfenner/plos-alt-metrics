3d433e33c121fc001a9ba7d24b5e79ef63e4f84a
Martin Fenner <fenner.martin@mh-hannover.de>
2012-03-06 19:14:51 +0000

    Simplified code for likes and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,31 +2,17 @@ class CommentsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :load_work
 
+  def index
+    respond_to do |format|
+      format.js { render "works/work" }
+    end
+  end
+  
   def new
     @comment = Comment.new
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]
     
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end
   end
   
@@ -34,58 +20,16 @@ class CommentsController < ApplicationController
     @comment = Comment.new(params[:comment])
     @work.comments << @comment unless @comment.text.blank?
     
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]
-    
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end
   end
   
   def edit
     @comment = Comment.find(params[:id])
     
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]
-    
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end
   end
   
@@ -94,29 +38,8 @@ class CommentsController < ApplicationController
     @comment.update_attributes(params[:comment])
     @comment.destroy if @comment.text.blank?
     
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]
-    
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end
   end
 
@@ -124,29 +47,8 @@ class CommentsController < ApplicationController
     @comment = Comment.find(params[:id])
     @comment.destroy
     
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]
-    
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end  
   end
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,29 +6,8 @@ class LikesController < ApplicationController
     @like = Like.new(:user_id => current_user.id, :work_id => @work.id)
     @work.likes << @like
     
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]
-    
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end
   end
   
@@ -36,29 +15,8 @@ class LikesController < ApplicationController
     @like = Like.find(params[:id])
     @like.destroy
     
-    if !params[:q].blank?
-      @works = Work.paginate :page => params[:page], 
-        :per_page => 10,
-        :include => :users,
-        :conditions => ["users.name REGEXP ? or users.username REGEXP ? or users.native_name REGEXP ? or works.title REGEXP ? or works.doi REGEXP ?", params[:q], params[:q], params[:q], params[:q], params[:q]]
-    else
-      collection = Work
-      collection = collection.cited(params[:cited])  if params[:cited]
-      collection = collection.query(params[:query])  if params[:query]
-      collection = collection.order(params[:order])  if params[:order]
-      
-      if user_signed_in?
-        # Fetch all works by the users you are following
-        @works = collection.paginate :conditions => ["FIND_IN_SET(contributors.user_id, '?')",current_user.friends], :include => [:users, :contributors], :page => params[:page], :per_page => 10
-      else
-        @works = collection.paginate :page => params[:page], :per_page => 10
-      end
-    end
-    
-    @source = Source.find_by_type(params[:source]) if params[:source]  
-    
     respond_to do |format|
-      format.js
+      format.js { render "works/work" }
     end
   end
   
